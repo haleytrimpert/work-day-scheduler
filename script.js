bd0ef89b70b86a3dbc12470209dba7517135f42c
Haley T <hntrimpert@yahoo.com>
2023-01-30 21:59:37 -0500

    local storage for save button

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ $(function () {
     var time = dayjs().hour();
     $(".time-block").each(function(){
       var hour = parseInt($(this).attr('id').substring(5));
-      
+      var stringHour=$(this).attr('id').substring(5)
+      var loadedText=localStorage.getItem(stringHour);
+      this.children[1].value= loadedText;
       if(time===hour){
         $(this).addClass("present");
       }else if(time < hour){
@@ -23,7 +25,15 @@ $(function () {
   changingTime();
 
 $(".saveBtn").on('click', function() {
-  console.log('click');
+ 
+  var eventTime = ($(this.parentElement).attr('id').substring(5));
+  
+  var eventText = this.parentElement.children[1].value;
+
+  localStorage.setItem(eventTime, eventText);
+ console.log(localStorage.getItem(eventTime));
+ 
+
 })
  
 
